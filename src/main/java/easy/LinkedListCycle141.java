230b5f9eaa14aedcb518f1aba88623d35c2f54f5
Nazem Elmadani <nazem.hem@gmail.com>
2023-01-06 20:36:26 +0200

    141. Linked List Cycle

---
+++ src/main/java/easy/LinkedListCycle141.java
@@ -0,0 +1,39 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * <p>
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+ * Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+ * <p>
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ */
+/*
+Runtime 11 ms Beats 8.69%
+Memory 47.2 MB Beats 8.51%
+ */
+public class LinkedListCycle141 {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node2;
+        System.out.println("Contains a cycle? " + hasCycle(node1));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) return true;
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
